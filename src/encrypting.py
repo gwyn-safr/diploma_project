ed2d23c67b6cf7b58d9bd0f5c56501aa25ffd81f
Artem Yulkubaiev <aiulkubaiev@mail.ru>
2024-05-29 21:32:06 +0300

    split main functions from key operation

--- src/encrypting.py
+++ src/encrypting.py
@@ -1,39 +1,32 @@
-from ecies.utils import generate_eth_key, generate_key
+import ecies
+from ecies.utils import generate_eth_key
 from ecies import encrypt, decrypt
 
 import chardet
 
-def encrypt_data(message, key) -> bytes:
+def encrypt_data(message, public_key) -> bytes:
   """
   Function transforms string input (message) into bytes
   to allow ecies library to function properly
   Input: string, eth_keys.datatypes.PrivateKey
   Return: <class 'bytes'> encrypted message
   """
-  # eth_k.public_key.to_hex()  # hex string
-  return encrypt(key.public_key.to_hex(), message)
+  return encrypt(public_key, message)
 
-def decrypt_data(message, key) -> bytes:
-  return decrypt(key.to_hex(), encrypt(key.public_key.to_hex(), message))
+def decrypt_data(encrypted_message, private_key) -> bytes:
+  return decrypt(private_key, encrypted_message)
 
-def generate_encr_key():
+def generate_key():
   return generate_eth_key()
 
+def create_public_key(key):
+  return key.public_key.to_hex()
+
+def create_private_key(key):
+  return key.to_hex()
+
 if __name__ == "__main__":
-  eth_k = generate_eth_key()
-  sk_hex = eth_k.to_hex()  # hex string
-  pk_hex = eth_k.public_key.to_hex()  # hex string
-
-  # https://chardet.readthedocs.io/en/latest/usage.html#example-using-the-detect-function
-  data = bytes('this is a test', 'UTF-8') # each value in string is supposed to be a hex number
-  print(chardet.detect(data))
-
-  decrypt(sk_hex, encrypt(pk_hex, data))
-
-  secp_k = generate_key()
-  sk_bytes = secp_k.secret  # bytes
-  pk_bytes = secp_k.public_key.format(True)  # bytes
-  decrypt(sk_bytes, encrypt(pk_bytes, data))
-
-  print(type(decrypt_data(data, eth_k)))
-  print(decrypt(sk_bytes, encrypt(pk_bytes, data)))
+  key = generate_key()
+  cypher = encrypt_data(b'test', create_public_key(key))
+  print("test: ", cypher)
+  print(decrypt_data(cypher, create_private_key(key)))
